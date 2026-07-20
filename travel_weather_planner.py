6a8126fb6f13ee86da6a3218a6a26f21b0e41188
GabSC016 <gcunha719@hotmail.com>
2026-07-20 22:04:38 +0000

    lab_01

---
+++ travel_weather_planner.py
@@ -0,0 +1,27 @@
+# Project: Build a Travel Weather Planner
+
+# For this lab, you will use conditional statements to determine whether commuting is possible based on the weather, the distance to travel, and the availability of a vehicle.
+
+distance_mi = 3
+is_raining = False
+has_bike = True
+has_car = False
+has_ride_share_app = False
+
+if not distance_mi:
+    print(False)
+elif distance_mi <= 1:
+    if not is_raining:
+        print(True)
+    else:
+        print(False)
+elif distance_mi <= 6:
+    if has_bike and not is_raining:
+        print(True)
+    else:
+        print(False)
+else:
+    if has_car or has_ride_share_app:
+        print(True)
+    else:
+        print(False)
